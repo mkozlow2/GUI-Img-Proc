09bab0f3dca36a5735a09e4f6e48b55cdeb4353d
Mateusz Kozlowski <mkozlow2@gmail.com>
2018-04-26 04:08:35 +0200

    Utworzone strony interfejsu; zarys wyglądu stron; zachowane proporcje okna podczas zmiany wymiarów

--- Interfejs/MainWindow.xaml.cs
+++ Interfejs/MainWindow.xaml.cs
@@ -22,7 +22,29 @@ namespace Interfejs
     {
         public MainWindow()
         {
+            this.Width = (System.Windows.SystemParameters.PrimaryScreenWidth * 0.5);
+            this.Height = (System.Windows.SystemParameters.PrimaryScreenHeight * 0.5);
             InitializeComponent();
+        }
+
+        private void p1Selected(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+
+        private void p2Selected(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page2();
+        }
+
+        private void p3Selected(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page3();
+        }
+
+        private void Main_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            this.Height = this.Width * (System.Windows.SystemParameters.PrimaryScreenHeight * 0.5) / (System.Windows.SystemParameters.PrimaryScreenWidth * 0.5);
         }
     }
 }
---
+++ Interfejs/Page1.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Interfejs
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtPar1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            double par;
+            if (!double.TryParse(txtPar1.Text, out par))
+            {
+                lblAlert.Foreground = Brushes.Red;
+                lblAlert.Content = "Podana nieprawidłowa wartość";
+            }
+            else
+            {
+                lblAlert.Foreground = Brushes.Black;
+                lblAlert.Content = null;
+            }
+
+        }
+
+        private void txtPar2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            double par;
+            if (!double.TryParse(txtPar1.Text, out par))
+            {
+                lblAlert.Foreground = Brushes.Red;
+                lblAlert.Content = "Podana nieprawidłowa wartość";
+            }
+            else
+            {
+                lblAlert.Foreground = Brushes.Black;
+                lblAlert.Content = null;
+            }
+        }
+    }
+}
